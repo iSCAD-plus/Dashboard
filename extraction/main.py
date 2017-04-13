373b581f6c9cc1078d810817a17136e361ba288d
Nicholas Tietz <ntietz@gmail.com>
2017-04-13 09:30:07 -0400

    Removed commented out code

--- extraction/main.py
+++ extraction/main.py
@@ -48,41 +48,6 @@ for row in range(1, decisionsWorksheet.nrows):
     for error in errors:
       (fatal, etype, msg) = error
       print('{etype}, row {row}: {msg}'.format(etype=etype, row=row, msg=msg))
-  #date1 = rowvals[3]
-  #date2 = xldate.xldate_as_datetime(rowvals[4], 0).strftime('%d %B %Y')
-  #if date2[0] == '0':
-  #  date2 = date2[1:]
-  #if date1 != date2:
-  #  print('date mismatch in row {row}: {d1} != {d2}'.format(row=row+1, d1=date1, d2=date2))
-  #  numBad += 1
-  #if rowvals[2] != float(xldate.xldate_as_datetime(rowvals[4],0).year):
-  #  print('date mismatch in row {row}: {y1} != {y2}'.format(row=row+1, y1=rowvals[2], y2=xldate.xldate_as_datetime(rowvals[4],0).year))
-  #  numBad += 1
-
-  #  decision = rowvals[0]
-  #  regime = rowvals[1]
-  #  year = rowvals[2] # TODO: remove eventually
-  #  date = xldate.xldate_as_datetime(rowvals[4], 0)
-  #  numParagraphs = rowvals[5]
-  #  decisionType = rowvals[6].strip().lower()
-  #
-  #  measures = []
-  #  for col in range(7, decisionsWorksheet.ncols):
-  #    if (rowvals[col]) != 0.0:
-  #      measures.append({'measureType': rowvals[col].lower(), 'measureCategory': header[col].lower()})
-
-  #  if decisionType not in set(["extend", "implementation", "establish", "exemption", "intention", "terminate"]):
-  #    print('decision type not recogniized in row {row}: {decision}'.format(decision=decisionType, row=row+1))
-
-  #  body = {
-  #    'decision': decision,
-  #    'regime': regime,
-  #    'year': year,
-  #    'date': date.timestamp() * 1000,
-  #    'numParagraphs': numParagraphs,
-  #    'decisionType': decisionType,
-  #    'measures': measures
-  #  }
 
   body = decisionsExtractor.process_row(rowvals)
   req = requests.post('http://localhost:3000/graphql', json={'query': insertQuery, 'variables': {'dec': body}})
