772f7276c35e5b76b4c5ed3d28a303ad557b8ef6
Nicholas Tietz <ntietz@gmail.com>
2017-04-19 15:02:24 -0400

    Trying some aggregations

--- src/server/query.js
+++ src/server/query.js
@@ -1,15 +1,33 @@
+import schemas from '../schema';
+
 export const decisionQuery = (obj, args, context, resolveInfo) => {
   console.log('obj: ');
   console.log(obj);
   console.log('\n\n');
   console.log('resolveInfo: ');
-  //const names = resolveInfo.operation.selectionSet.selections.map(
+  // const names = resolveInfo.operation.selectionSet.selections.map(
   //  selection => selection.name.value
-  //);
+  // );
   const names2 = resolveInfo.operation.selectionSet.selections[
     0
   ].selectionSet.selections.map(selection => selection.name.value);
   console.log(names2);
   console.log('\n\n');
+
+  console.log(
+    schemas.Decision
+      .aggregate([
+        {
+          $group: {
+            _id: '$year',
+            year: { $first: '$year' },
+            count: { $sum: 1 },
+          },
+        },
+      ])
+      .exec()
+      .then(x => console.log(x))
+  );
+
   return null;
 };
