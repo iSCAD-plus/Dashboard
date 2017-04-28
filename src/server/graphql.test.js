6bc6a981b8037dea487b0f7ac9c8c7a0c0f93afb
Nicholas Tietz <ntietz@gmail.com>
2017-04-28 10:25:04 -0400

    Verified that the global count works

--- src/server/graphql.test.js
+++ src/server/graphql.test.js
@@ -1,11 +1,45 @@
 import mongoose from 'mongoose';
 import { graphql } from 'graphql';
 import R from 'ramda';
+import jsc from 'jsverify';
 import resolverMap from './graphql';
-import schemas from '../schema';
+import schemas, {
+  decisionTypes,
+  measureCategories,
+  measureTypes,
+} from '../schema';
 
 mongoose.Promise = Promise;
 mongoose.connect('localhost', 'iscad-test-temp');
+
+const decisionSpec = jsc.record({
+  decision: jsc.nestring,
+  regime: jsc.nestring,
+  year: jsc.integer,
+  date: jsc.datetime,
+  numParagraphs: jsc.nat,
+  decisionType: jsc.elements(decisionTypes),
+  measures: jsc.array(
+    jsc.record({
+      measureCategory: jsc.elements(measureCategories),
+      measureType: jsc.elements(measureTypes),
+    })
+  ),
+});
+const decisionMaker = jsc.sampler(decisionSpec);
+
+const addDecisions = async (numDecisions) => {
+  const promises = decisionMaker(numDecisions).map(each =>
+    new schemas.Decision(each).save()
+  );
+  await Promise.all(promises);
+};
+
+const countQuery = `
+  query Q {
+    countDecisions
+  }
+`;
 
 beforeEach(async () => {
   const onError = (err) => {
@@ -38,14 +72,18 @@ test('Empty DB gives empty query', async () => {
 });
 
 test('Empty DB should return count of 0', async () => {
-  const query = `
-    query Q {
-      countDecisions
-    }
-  `;
-
-  const result = await graphql(schemas.graphql, query, resolverMap.Query);
+  const result = await graphql(schemas.graphql, countQuery, resolverMap.Query);
   const expectedResult = { data: { countDecisions: 0 } };
+
+  expect(result).toEqual(expectedResult);
+});
+
+test('Populated DB returns the correct count', async () => {
+  const numDecisions = 100;
+  await addDecisions(numDecisions);
+
+  const result = await graphql(schemas.graphql, countQuery, resolverMap.Query);
+  const expectedResult = { data: { countDecisions: numDecisions } };
 
   expect(result).toEqual(expectedResult);
 });
