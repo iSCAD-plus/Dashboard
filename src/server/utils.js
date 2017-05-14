d914cd445e3899636136e7b69aa335be3172d5fc
ntietz <ntietz@gmail.com>
2017-05-14 00:13:34 -0400

    Add average length

--- src/server/utils.js
+++ src/server/utils.js
@@ -10,12 +10,14 @@ const prepend = R.ifElse(
 
 const aggregationOperator = R.cond([
   [R.equals('count'), R.always({ $sum: 1 })],
+  [R.equals('averageLength'), R.always({ $avg: prepend('numParagraphs') })],
   [R.equals('month'), R.always({ $first: { $month: prepend('date') } })],
   [R.T, key => ({ $first: prepend(key) })],
 ]);
 
 const formId = R.cond([
   [R.equals('count'), R.always('')],
+  [R.equals('averageLength'), R.always('')],
   [
     R.equals('month'),
     R.always({ $substr: [{ $month: prepend('date') }, 0, -1] }),
